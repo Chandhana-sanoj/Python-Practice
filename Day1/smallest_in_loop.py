2b3333dd5d417a617e87a4e6c0ca2e83af29ec5b
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 18:20:36 +0530

    Update smallest_in_loop.py

--- Day1/smallest_in_loop.py
+++ Day1/smallest_in_loop.py
@@ -1,1 +1,10 @@
+#find the smallest number from list [9,41,12,3,74,15]
 
+smallest = None
+for value in [9,41,12,3,74,15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+print('Smallest number is', smallest)   
+    

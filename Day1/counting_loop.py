e1239232e02576bc6e4965233242626e1654a5fd
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 17:11:34 +0530

    Update counting_loop.py

--- Day1/counting_loop.py
+++ Day1/counting_loop.py
@@ -1,1 +1,16 @@
+#count the numbers of list [9,41,12,3,74,15]
+count=0
+print('before',count)
+for number in [9,41,12,3,74,15]:
+    count = count + 1  
+    print(count,number) 
+print('after',count)
 
+
+
+#count the even numbers of list [9,41,12,3,74,15]
+count=0
+for number in [9,41,12,3,74,15]:
+    if number%2 == 0:
+     count = count + 1  
+    print(count,number) 

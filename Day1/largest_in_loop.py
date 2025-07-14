1a3fbe8c03e84cb8dd066333ea6f3434829b8f8b
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 18:24:19 +0530

    Update largest_in_loop.py

--- Day1/largest_in_loop.py
+++ Day1/largest_in_loop.py
@@ -1,1 +1,9 @@
+# Find the largest number in the list [9, 41, 12, 3, 74, 15]
 
+largest = None
+for value in [9,41,12,3,74,15]:
+    if largest is None:
+        largest = value
+    elif value > largest:
+        largest = value
+print('largest number is', largest

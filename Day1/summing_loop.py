dada919a448347340473e5d535cda0c1b72313ef
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 17:26:19 +0530

    Update summing_loop.py

--- Day1/summing_loop.py
+++ Day1/summing_loop.py
@@ -1,1 +1,18 @@
+#sum the numbers of list [9,41,12,3,74,15]
+sum=0
+print('before', sum)
+for number in [9,41,12,3,74,15]:
+     sum = sum + number 
+     print(sum,number) 
+print('after', sum)
 
+
+
+#sum the even numbers of list [9,41,12,3,74,15]
+sum=0
+print('before', sum)
+for number in [9,41,12,3,74,15]:
+    if number%2 == 0:
+     sum = sum + number 
+     print(sum,number) 
+print('after', sum)

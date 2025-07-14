a8c76931ff8f858bc3220270ee6de0c2759a1a26
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 17:35:34 +0530

    Update average_loop.py

--- Day1/average_loop.py
+++ Day1/average_loop.py
@@ -1,1 +1,10 @@
+#average the numbers of list [9,41,12,3,74,15]
 
+count=0
+sum=0
+print('before',count, sum)
+for number in [9,41,12,3,74,15]:
+     count = count + 1
+     sum = sum + number
+     print(count, sum, number)     
+print('after',count,sum, sum/count)

0f91f47b3d3bc638542f97abf8f3d04eabfffee6
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 17:37:18 +0530

    Update average_loop.py

--- Day1/average_loop.py
+++ Day1/average_loop.py
@@ -8,3 +8,16 @@ for number in [9,41,12,3,74,15]:
      sum = sum + number
      print(count, sum, number)     
 print('after',count,sum, sum/count)
+
+
+
+#average only even numbers of list [9,41,12,3,74,15]
+count=0
+sum=0
+print('before',count, sum)
+for number in [9,41,12,3,74,15]:
+    if number%2 == 0:
+     count = count + 1
+     sum = sum + number
+     print(count, sum, number)     
+print('after',count,sum, sum/count)

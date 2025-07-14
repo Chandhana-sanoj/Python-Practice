e73b80fa8aa63ef7d11ab676bc2f710841e9a0c6
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-14 18:44:24 +0530

    Update search_boolean.py

--- Day1/search_boolean.py
+++ Day1/search_boolean.py
@@ -1,1 +1,8 @@
+# Search for 12 in the list [9, 41, 12, 3, 74, 15] using a boolean flag
 
+found=False
+for value in [9,41,12,3,74,15]:
+    if value == 12:
+        found = True
+        break
+print(found,value)
